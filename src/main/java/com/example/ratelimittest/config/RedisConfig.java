1877f21145511d94543b13c891db2858a99bb4fa
frank.oh <frank.oh@kakaohealthcare.com>
2023-05-25 00:02:38 +0900

    redis bucket4j 기본설정

--- src/main/java/com/example/ratelimittest/config/RedisConfig.java
+++ src/main/java/com/example/ratelimittest/config/RedisConfig.java
@@ -1,62 +1,62 @@
 package com.example.ratelimittest.config;
 
 import io.github.bucket4j.Bandwidth;
-import io.github.bucket4j.Bucket;
 import io.github.bucket4j.BucketConfiguration;
 import io.github.bucket4j.distributed.ExpirationAfterWriteStrategy;
+import io.github.bucket4j.distributed.proxy.RemoteBucketBuilder;
+import io.github.bucket4j.distributed.serialization.Mapper;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.cluster.RedisClusterClient;
+
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
-import lombok.Getter;
 
 public class RedisConfig {
 
-  private static final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
-  @Getter
-  private static Bucket bucket;
+    public RemoteBucketBuilder getRemoteBucketBuilder() {
+        RedisClusterClient redisClusterClient = redisClusterClient();
+        LettuceBasedProxyManager proxyManager = LettuceBasedProxyManager.builderFor(redisClusterClient)
+                .withKeyMapper(Mapper.STRING)
+                .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
+                        Duration.ofHours(1)))
+                .build();
 
-  public void redisRateLimitConfig() {
-    RedisClusterClient redisClusterClient = redisClusterClient();
-    LettuceBasedProxyManager proxyManager = LettuceBasedProxyManager.builderFor(redisClusterClient)
-        .withExpirationStrategy(ExpirationAfterWriteStrategy.basedOnTimeForRefillingBucketUpToMax(
-            Duration.ofSeconds(10)))
-        .build();
+        return proxyManager.builder().withMapper(key -> "LIMIT::CHEERING::" + key);
+    }
 
-    BucketConfiguration configuration = BucketConfiguration.builder()
-        .addLimit(Bandwidth.simple(1_000, Duration.ofMinutes(1)))
-        .build();
-    bucket = proxyManager.builder().build("cheering", configuration);
-  }
+    public static BucketConfiguration getBucketConfiguration() {
+        BucketConfiguration configuration = BucketConfiguration.builder()
+                .addLimit(Bandwidth.simple(10, Duration.ofHours(1)))
+                .build();
+        return configuration;
+    }
 
-  private List<String> nodes = Arrays.asList("127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003",
-      "127.0.0.1:7004", "127.0.0.1:7005", "127.0.0.1:7006"); // replace with your nodes
+    private List<String> nodes = Arrays.asList("127.0.0.1:7001", "127.0.0.1:7002", "127.0.0.1:7003",
+            "127.0.0.1:7004", "127.0.0.1:7005", "127.0.0.1:7006"); // replace with your nodes
 
-  public RedisClusterClient redisClusterClient() {
-    List<RedisURI> redisURIs = getRedisURIs();
-    return RedisClusterClient.create(redisURIs);
-  }
+    public RedisClusterClient redisClusterClient() {
+        List<RedisURI> redisURIs = getRedisURIs();
+        return RedisClusterClient.create(redisURIs);
+    }
 
-  private List<RedisURI> getRedisURIs() {
-    return nodes.stream()
-        .map(node -> node.split(":"))
-        .map(this::toRedisURI)
-        .collect(Collectors.toList());
-  }
+    private List<RedisURI> getRedisURIs() {
+        return nodes.stream()
+                .map(node -> node.split(":"))
+                .map(this::toRedisURI)
+                .collect(Collectors.toList());
+    }
 
-  private RedisURI toRedisURI(String[] node) {
-    String host = node[0];
-    int port = Integer.parseInt(node[1]);
-    return RedisURI.builder()
-        .withHost(host)
-        .withPort(port)
-        .withTimeout(Duration.ofSeconds(60)) // adjust as needed
-        .build();
-  }
+    private RedisURI toRedisURI(String[] node) {
+        String host = node[0];
+        int port = Integer.parseInt(node[1]);
+        return RedisURI.builder()
+                .withHost(host)
+                .withPort(port)
+                .withTimeout(Duration.ofSeconds(60)) // adjust as needed
+                .build();
+    }
 
 }
--- src/test/java/com/example/ratelimittest/service/RateLimitServiceTest.java
+++ src/test/java/com/example/ratelimittest/service/RateLimitServiceTest.java
@@ -1,38 +1,48 @@
 package com.example.ratelimittest.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.example.ratelimittest.config.RedisConfig;
-import io.github.bucket4j.Bucket;
+import io.github.bucket4j.BucketConfiguration;
+import io.github.bucket4j.distributed.BucketProxy;
+import io.github.bucket4j.distributed.proxy.RemoteBucketBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.StopWatch;
 
 class RateLimitServiceTest {
 
-  @Test
-  void test() throws Exception {
-    RateLimitService rateLimitService = new RateLimitService();
-    StopWatch stopWatch = new StopWatch();
-    stopWatch.start();
-    for (int i = 0; i < 30; i++) {
+    @Test
+    void localTest() throws Exception {
+        RateLimitService rateLimitService = new RateLimitService();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < 30; i++) {
 
-      rateLimitService.test();
-      stopWatch.stop();
-      System.out.println(stopWatch.getTotalTimeMillis());
-      stopWatch.start();
-      Thread.sleep(1000);
+            rateLimitService.test();
+            stopWatch.stop();
+            System.out.println(stopWatch.getTotalTimeMillis());
+            stopWatch.start();
+            Thread.sleep(1000);
+        }
     }
-  }
 
-  @Test
-  void redisTest() throws Exception {
+    @Test
+    void redisTest() throws Exception {
 
-    RedisConfig redisConfig = new RedisConfig();
-    redisConfig.redisRateLimitConfig();
+        RedisConfig redisConfig = new RedisConfig();
+        RemoteBucketBuilder remoteBucketBuilder = redisConfig.getRemoteBucketBuilder();
+        BucketConfiguration bucketConfiguration = RedisConfig.getBucketConfiguration();
 
-    Bucket bucket = RedisConfig.getBucket();
-    System.out.println(bucket);
-    bucket.tryConsume(1);
-  }
+        BucketProxy build = remoteBucketBuilder.build(999, bucketConfiguration);
+
+        for (int i = 0; i < 15; i++) {
+
+            boolean b = build.tryConsume(1);
+
+            if (b) {
+                System.out.println(true);
+            } else {
+                System.out.println(false);
+            }
+        }
+    }
 
 }
